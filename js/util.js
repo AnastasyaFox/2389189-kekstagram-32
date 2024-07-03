1b13c3074d7d29420a6fe0e21e389b44188625c7
AnastasyaFox <157881945+AnastasyaFox@users.noreply.github.com>
2024-07-03 14:26:46 +0300

    модули

---
+++ js/util.js
@@ -0,0 +1,25 @@
+function fn (str, num) {
+  if (str.length <= num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(fn('моя первая строка', 20));
+console.log(fn('втораястрокатекста', 18));
+console.log(fn('Третья строка текста', 10));
+const checkPalindrome = (string) => {
+  const normalString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+  for (let i = normalString.length - 1; i >= 0; i--) {
+    reversedString += normalString[i];
+  }
+  return normalString === reversedString;
+}
+console.log(checkPalindrome('noon'));
+console.log(checkPalindrome('ДовОд'));
+console.log(checkPalindrome('Кекс'));
+console.log(checkPalindrome('Лёша на полке клопа нашёл '));
+
+
+export {getRandomInteger, stringCheck, isPalindrome, getNumbers};
