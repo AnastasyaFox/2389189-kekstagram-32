66aba5abb68fb0e7967005969e4fc49cfa568103
AnastasyaFox <157881945+AnastasyaFox@users.noreply.github.com>
2024-07-18 13:17:30 +0300

    открытие фото

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,27 +1,30 @@
-import { picture } from "./rendering-picture";
+import { picture as pictures } from './rendering-picture';
+const picturesContainer = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 const closeButtonPicture = bigPicture.querySelector('.big-picture__cancel');
 
 const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeBigPicture();
-  }
+// ///  if (isEscapeKey(evt)) {
+//     evt.preventDefault();
+//     closeBigPicture();
+//   }
 };
 
-function openBigPicture () {
+function openBigPicture (id) {
   bigPicture.classList.remove('hidden');
-  picture();
+  const curentPicture = pictures.filter((item) => item.id === Number(id))[0];
+  bigPicture.querySelector('.big-picture__img img').src = curentPicture.url;
+  // picture();
   document.addEventListener('keydown', onDocumentKeydown);
 }
 
-bigPicture.addEventListener('click', () => {
-  openBigPicture();
+picturesContainer.addEventListener('click', (evt) => {
+  openBigPicture(evt.target.dataset.id);
 });
 
 function closeBigPicture () {
   bigPicture.classList.add('hidden');
-  picture();
+  // picture();
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
